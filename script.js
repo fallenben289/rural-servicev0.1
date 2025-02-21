fdce3235e68d9ee3073f0ed3ece13ed0ce62bdcd
fallenben289 <65033733+fallenben289@users.noreply.github.com>
2025-02-21 21:16:15 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Function to open service modal
+  function openServiceModal() {
+    document.getElementById('serviceModal').style.display = 'flex';
+  }
+
+  // Function to close service modal
+  function closeModal() {
+    document.getElementById('serviceModal').style.display = 'none';
+  }
+
+  // Function to open why modal
+  function openWhyModal() {
+    document.getElementById('whyModal').style.display = 'flex';
+  }
+
+  // Function to close why modal
+  function closeWhyModal() {
+    document.getElementById('whyModal').style.display = 'none';
+  }
